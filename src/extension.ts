69f56c34a7254a5fe6f43d28335eeeaa417356e2
Wouter <github@intodevelopment.nl>
2021-08-29 13:09:57 +0200

    Upgraded the extension to the latest version of the VS Code extension template

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -10,7 +9,7 @@ var nightlight: Nightlight | null = null;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    
+
     initialize();
 
     // Register commands
